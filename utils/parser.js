73717b52b79e3132ea8ca7d80bd22d74597681ab
TangiLC <tangi@le-cadre.net>
2024-05-21 15:57:17 +0200

    doctrine-file -> utils

---
+++ utils/parser.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const parseFile = require("./parseFile");
+const parseFileContent = require("./parseFileContent");
+const Extractor = require("./Extractor");
+
+exports.parseFile = parseFile;
+exports.parseFileContent = parseFileContent;
+exports.Extractor = Extractor;
