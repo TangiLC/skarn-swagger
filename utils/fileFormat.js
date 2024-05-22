7ab6a6bdd3deb5bcaa428f2ac9889cd8f3c4e93e
TangiLC <tangi@le-cadre.net>
2024-05-22 22:52:52 +0200

    imports

--- utils/fileFormat.js
+++ utils/fileFormat.js
@@ -26,37 +26,49 @@ function fileFormat(comments, responseFormats) {
 		schemas = {};
 	for (let i in comments) {
 		let desc = parseDescription(comments);
-		if (i == 'tags') {
-			if (comments[i].length > 0 && comments[i][0]['title'] && comments[i][0]['title'] == 'typedef') {
+		if (i == "tags") {
+			if (
+				comments[i].length > 0 &&
+				comments[i][0]["title"] &&
+				comments[i][0]["title"] == "typedef"
+			) {
 				const typedefParsed = parseTypedef(comments[i]);
 				components[typedefParsed.typeName] = typedefParsed.details;
 				continue;
 			}
 			for (let j in comments[i]) {
-				let title = comments[i][j]['title'];
-				let type = comments[i][j]['type'];
+				let title = comments[i][j]["title"];
+				let type = comments[i][j]["type"];
 
 				//if(comments[i][j]['type'])console.log(comments[i][j]['type'].name)
-				if (title == 'route') {
-					route = parseRoute(comments[i][j]['description']);
+				if (title == "route") {
+					route = parseRoute(comments[i][j]["description"]);
 					let tag = parseTag(comments[i]);
 					parameters[route.uri] = parameters[route.uri] || {};
-					parameters[route.uri][route.method] = parameters[route.uri][route.method] || {};
-					if (route.method == 'get') {
-						parameters[route.uri][route.method]['parameters'] = [];
-						parameters[route.uri][route.method]['responses'] = parseReturn(comments[i], responseFormats);
+					parameters[route.uri][route.method] =
+						parameters[route.uri][route.method] || {};
+					if (route.method == "get") {
+						parameters[route.uri][route.method]["parameters"] = [];
+						parameters[route.uri][route.method]["responses"] = parseReturn(
+							comments[i],
+							responseFormats
+						);
 					}
-					if (route.method.charAt(0) == 'p') {
+					if (route.method.charAt(0) == "p") {
 						//put post patch
 						let schema = {
-							type: 'object',
+							type: "object",
 							properties: {},
 							required: [],
 						};
 
 						let modelName = null;
 						comments[i].forEach((comment) => {
-							if (comment.title === 'param' && comment.type && comment.type.name) {
+							if (
+								comment.title === "param" &&
+								comment.type &&
+								comment.type.name
+							) {
 								modelName = comment.type.name;
 							}
 						});
@@ -65,49 +77,52 @@ function fileFormat(comments, responseFormats) {
 							schema = { $ref: `#/components/schemas/${modelName}` };
 						}
 
-						parameters[route.uri][route.method]['requestBody'] = {
+						parameters[route.uri][route.method]["requestBody"] = {
 							content: {
-								'application/json': {
+								"application/json": {
 									schema: schema,
 								},
 							},
 						};
-						parameters[route.uri][route.method]['responses'] = parseReturn(comments[i], responseFormats);
+						parameters[route.uri][route.method]["responses"] = parseReturn(
+							comments[i],
+							responseFormats
+						);
 					}
-					parameters[route.uri][route.method]['description'] = desc;
-					parameters[route.uri][route.method]['tags'] = [tag[0].trim()];
+					parameters[route.uri][route.method]["description"] = desc;
+					parameters[route.uri][route.method]["tags"] = [tag[0].trim()];
 					tags.push({
-						name: typeof tag[0] === 'string' ? tag[0].trim() : '',
-						description: typeof tag[1] === 'string' ? tag[1].trim() : '',
+						name: typeof tag[0] === "string" ? tag[0].trim() : "",
+						description: typeof tag[1] === "string" ? tag[1].trim() : "",
 					});
 				}
-				if (title == 'param') {
-					let field = parseField(comments[i][j]['name']),
+				if (title == "param") {
+					let field = parseField(comments[i][j]["name"]),
 						properties = {
 							name: field.name,
 							in: field.parameter_type,
-							description: comments[i][j]['description'],
+							description: comments[i][j]["description"],
 							required: field.required,
 						},
-						schema = parseSchema(comments[i][j]['type']);
+						schema = parseSchema(comments[i][j]["type"]);
 					// we only want a type if there is no referenced schema
 					if (!schema) {
 						properties.schema = {
-							type: parseType(comments[i][j]['type']),
-							default: comments[i][j]['default'],
+							type: parseType(comments[i][j]["type"]),
+							default: comments[i][j]["default"],
 						};
-						if (properties.schema.type == 'enum') {
-							let parsedEnum = parseEnums(comments[i][j]['description']);
+						if (properties.schema.type == "enum") {
+							let parsedEnum = parseEnums(comments[i][j]["description"]);
 							properties.schema.type = parsedEnum.type;
 							properties.schema.enum = parsedEnum.enums;
-						} else if (properties.schema.type == 'boolean') {
+						} else if (properties.schema.type == "boolean") {
 							let boolean = [true, false];
 							properties.schema.enum = boolean;
 						}
 					} else if (schema) {
 						properties.schema = schema;
-						if (type && type.name == 'enum') {
-							let parsedEnum = parseEnums(comments[i][j]['description']);
+						if (type && type.name == "enum") {
+							let parsedEnum = parseEnums(comments[i][j]["description"]);
 							properties.schema.type = parsedEnum.type;
 							properties.schema.enum = parsedEnum.enums;
 						}
@@ -115,38 +130,54 @@ function fileFormat(comments, responseFormats) {
 					params.push(properties);
 				}
 
-				if (title == 'operationId' && route) {
-					parameters[route.uri][route.method]['operationId'] = comments[i][j]['description'];
+				if (title == "operationId" && route) {
+					parameters[route.uri][route.method]["operationId"] =
+						comments[i][j]["description"];
 				}
 
-				if (title == 'summary' && route) {
-					parameters[route.uri][route.method]['summary'] = comments[i][j]['description'];
+				if (title == "summary" && route) {
+					parameters[route.uri][route.method]["summary"] =
+						comments[i][j]["description"];
 				}
 
-				if (title == 'produces' && route) {
-					parameters[route.uri][route.method]['produces'] = parseProduces(comments[i][j]['description']);
+				if (title == "produces" && route) {
+					parameters[route.uri][route.method]["produces"] = parseProduces(
+						comments[i][j]["description"]
+					);
 				}
 
-				if (title == 'consumes' && route) {
-					parameters[route.uri][route.method]['consumes'] = parseConsumes(comments[i][j]['description']);
+				if (title == "consumes" && route) {
+					parameters[route.uri][route.method]["consumes"] = parseConsumes(
+						comments[i][j]["description"]
+					);
 				}
 
-				if (title == 'security' && route) {
-					parameters[route.uri][route.method]['security'] = parseSecurity(comments[i][j]['description']);
+				if (title == "security" && route) {
+					parameters[route.uri][route.method]["security"] = parseSecurity(
+						comments[i][j]["description"]
+					);
 				}
 
-				if (title == 'deprecated' && route) {
-					parameters[route.uri][route.method]['deprecated'] = true;
+				if (title == "deprecated" && route) {
+					parameters[route.uri][route.method]["deprecated"] = true;
 				}
 
 				if (route) {
-					parameters[route.uri][route.method]['parameters'] = params;
-					parameters[route.uri][route.method]['responses'] = parseReturn(comments[i], responseFormats);
+					parameters[route.uri][route.method]["parameters"] = params;
+					parameters[route.uri][route.method]["responses"] = parseReturn(
+						comments[i],
+						responseFormats
+					);
 				}
 			}
 		}
 	}
-	return { parameters: parameters, tags: tags, components: components, schemas: schemas };
+	return {
+		parameters: parameters,
+		tags: tags,
+		components: components,
+		schemas: schemas,
+	};
 }
 
-module.exports = fileFormat;
+module.exports = { fileFormat };
